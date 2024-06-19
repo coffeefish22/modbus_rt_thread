9b2f7c72505f7d537050b0c454f3a3b4e7442886
coffeefish22 <55184769+coffeefish22@users.noreply.github.com>
2024-06-19 21:17:43 +0800

    Update mbrtu_m.c

--- mbrtu_m.c
+++ mbrtu_m.c
@@ -1,32 +1,3 @@
-/* 
- * FreeModbus Libary: A portable Modbus implementation for Modbus ASCII/RTU.
- * Copyright (c) 2013 China Beijing Armink <armink.ztl@gmail.com>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * File: $Id: mbrtu_m.c,v 1.60 2013/08/17 11:42:56 Armink Add Master Functions $
- */
 
 /* ----------------------- System includes ----------------------------------*/
 #include "stdlib.h"
@@ -346,7 +317,7 @@ xMBMasterRTUTransmitFSM( void )
             /* Disable transmitter. This prevents another transmit buffer
              * empty interrupt. */
 
-	//´Ë´¦µÄÑÓÊ±ÊÇÕë¶Ô485£¬Ä¿Ç°ÓÃµÄRS232¹ÊÒªÈ¥µô´Ë´¦µÄÑÓÊ±
+	//æ­¤å¤„çš„å»¶æ—¶æ˜¯é’ˆå¯¹485ï¼Œç›®å‰ç”¨çš„RS232æ•…è¦åŽ»æŽ‰æ­¤å¤„çš„å»¶æ—¶
 //   		i++;
 //		if(i>100)
 			{
