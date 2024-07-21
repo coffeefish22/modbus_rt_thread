938ba0b45eb79c2177d187a05eac58efdc253b13
coffeefish22 <55184769+coffeefish22@users.noreply.github.com>
2024-07-21 22:19:21 +0800

    Update port.h

--- port.h
+++ port.h
@@ -1,24 +1,3 @@
-/*
- * FreeModbus Libary: BARE Port
- * Copyright (C) 2006 Christian Walter <wolti@sil.at>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * File: $Id: port.h ,v 1.60 2013/08/13 15:07:05 Armink add Master Functions $
- */
-
 #ifndef _PORT_H
 #define _PORT_H
 
@@ -39,7 +18,7 @@
 #define COVER_DELAY		60000
 
 #if (USE_SERIAL_X_FOR_SLAVE== 3)
-//TODO  暂时先写B13引脚，等组网测试时再确认
+//TODO  鏆傛椂鍏堝啓B13寮曡剼锛岀瓑缁勭綉娴嬭瘯鏃跺啀纭
 #define SLAVE_RS485_SEND_MODE  do{GPIO_SetBits(GPIO_RS485_COM_SLAVE,PIN_RS485_COM_SLAVE);}while(0)//rt_hw_us_delay(200000);
 #define SLAVE_RS485_RECEIVE_MODE 	do{GPIO_ResetBits(GPIO_RS485_COM_SLAVE,PIN_RS485_COM_SLAVE);}while(0)//rt_hw_us_delay(10000);
 #else
